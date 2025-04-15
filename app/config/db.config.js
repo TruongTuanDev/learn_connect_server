1d095c35eaba0c65efbf48300dd83cc7c1a04054
phuc1st <tanphongtp7@gmail.com>
2025-04-15 22:00:21 +0700

    feat: implement chatting 1-1

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,5 +2,5 @@ module.exports = {
   HOST: "localhost",
   PORT: 27017,
   DB: "learn_connect_server",
-  url: "mongodb://localhost:27017/learn_connect_server",
+  url: "mongodb://root:root@localhost:27017/learn_connect_server?authSource=admin",
 };
--- app/models/index.js
+++ app/models/index.js
@@ -8,6 +8,7 @@ db.mongoose = mongoose;
 
 db.user = require("./user.model");
 db.role = require("./role.model");
+db.message = require("./message.model");
 
 db.ROLES = ["user","moderator","admin"];
 

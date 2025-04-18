4683f682ea6c72aa94963195457d7b08bd6cadc2
TruongTuanDev <tuanhulonhat@gmail.com>
2025-04-18 09:21:14 +0700

    fix

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ const authConfig = require("./app/config/auth.config");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8080",
+  origin: ["http://127.0.0.1:5500", "http://localhost:5500"], // thêm frontend port
   methods: ['GET', 'POST'],
+  credentials: true
 }; 
 
 app.use(cors(corsOptions));
